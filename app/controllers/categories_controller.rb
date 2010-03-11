69a7e190810bb5046ac73f4b5db2a4b5e7b90225
gra <vk@rails.in.ua>
2010-03-11 23:03:37 +0200

    implement filters logic fix

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,7 @@ class CategoriesController < ApplicationController
       @category = Category.find_by_permalink params[:permalink]
       @items = @category.items.published.for_size(params[:size]).and_gender(params[:gender]).price_range(params[:price_range]) if @category
     else
-      @items = Item.published
+      @items = Item.published.for_size(params[:size]).and_gender(params[:gender]).price_range(params[:price_range])
     end
     
   end
