fc21604ac943bdb7cab403cafb94955bcfda63f4
gra <vk@rails.in.ua>
2010-03-11 19:38:39 +0200

    no border in similar items

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,12 +3,3 @@ $(function(){
 
 
 });
-
-
-function set_no_border_for_last_list_in_similar_items()
-{
-	
-	var items = $("#similar-items li");
-	$(items[items.length-1]).addClass("no-border");
-	
-}
