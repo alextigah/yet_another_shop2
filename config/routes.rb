26e942670e8c5ec40cf212d924d99450445bc122
gra <vk@rails.in.ua>
2010-03-31 18:55:11 +0300

    page permalink fix

--- config/routes.rb
+++ config/routes.rb
@@ -20,9 +20,10 @@ ActionController::Routing::Routes.draw do |map|
     
   map.category 'shop/:permalink', :controller => 'categories'
   map.connect 'shop/:permalink/:id', :controller => 'items', :action => 'show'
+  map.page 'page/:permalink', :controller => 'pages', :action => 'show'
   
   map.resources :items    
-  map.resources :pages, :as => "page"
+  # map.resources :pages, :as => "page"
   map.resources :brands
   map.resources :order_items
   map.resource :orders, :member => {:success => :get, :payment_success => :get, :waiting_for_payment => :get, :payment_error => :get}
