82db17bd3110d6781500227184191be3f65e0ffd
gra <vk@rails.in.ua>
2010-03-31 13:39:52 +0300

    http auth to admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -4,4 +4,13 @@ class Admin::AdminController < InheritedResources::Base
   
   skip_before_filter :setup
   
+	before_filter :authenticate  
+  
+	private
+	def authenticate
+		authenticate_or_request_with_http_basic do |user_name, password|
+		user_name == "admin" && password == "ktotulen"
+		end
+	end  
+  
 end
