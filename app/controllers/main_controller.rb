0246a69dc442ba41c743bea1764f62c4aeb9fdd1
gra <vk@rails.in.ua>
2010-03-31 20:56:54 +0300

    twitter fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,9 @@ class MainController < ApplicationController
     @items = Item.published.limit(10)
 #    @sales = Sale.published.limit(3)
     @sale = Sale.published.last
-    httpauth = Twitter::HTTPAuth.new('_gra', 'zscezsce5')
-    client = Twitter::Base.new(httpauth)    
-    @messages = client.user_timeline(:id => "shuttle_ua", :count => 5)
+    # httpauth = Twitter::HTTPAuth.new('_gra', 'zscezsce5')
+    # client = Twitter::Base.new(httpauth)    
+    # @messages = client.user_timeline(:id => "shuttle_ua", :count => 5)
   end
 
 end
