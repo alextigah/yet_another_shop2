8bfedf13e86d40f5e1e8d5f68c50b0784ff80946
gra <vk@rails.in.ua>
2010-03-31 12:10:55 +0300

    item photos fix

--- app/models/item_photo.rb
+++ app/models/item_photo.rb
@@ -1,7 +1,7 @@
 class ItemPhoto < ActiveRecord::Base
 
   acts_as_list  
-  has_attached_file :image, :styles => { :thumb => "90x90#" }    
+  has_attached_file :image, :styles => { :thumb => "159x190", :small => "90x90", :large => "350x500" }  
   
   belongs_to :item
   
