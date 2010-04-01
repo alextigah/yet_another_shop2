d758ff4608c63c6df4405f4d81b5fe4e8613eedb
zakusha <zakusha@yandex.ru>
2010-04-02 02:19:24 +0300

    thump up

--- app/models/item_photo.rb
+++ app/models/item_photo.rb
@@ -1,7 +1,7 @@
 class ItemPhoto < ActiveRecord::Base
 
   acts_as_list  
-  has_attached_file :image, :styles => { :thumb => "160x200", :small => "90x113", :large => "400x500#" }  
+  has_attached_file :image, :styles => { :small => "90x113", :large => "400x500#" }  
   
   belongs_to :item
   
